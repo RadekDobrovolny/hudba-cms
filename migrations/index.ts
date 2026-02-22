d89e1c548c8284e4d7f39307747fdb9b3893d81f
Radek Dobrovolný <radek.dobrovolny@gmail.com>
2026-02-22 16:26:33 +0100

    migrations

---
+++ migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260222_152616 from './20260222_152616';
+
+export const migrations = [
+  {
+    up: migration_20260222_152616.up,
+    down: migration_20260222_152616.down,
+    name: '20260222_152616'
+  },
+];
